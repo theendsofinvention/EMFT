3d5d3542601ddec5cd73fdee7b43596e831e3037
132nd-etcher <132nd-etcher@daribouca.net>
2017-04-18 07:42:48 +0200

    pep8

--- src/misc/github.py
+++ src/misc/github.py
@@ -4,5 +4,6 @@ import requests
 
 
 def get_available_branches():
-    req = requests.get(r'https://api.github.com/repos/132nd-Entropy/132nd-Virtual-Wing-Training-Mission-Tblisi/branches')
+    req = requests.get(
+        r'https://api.github.com/repos/132nd-Entropy/132nd-Virtual-Wing-Training-Mission-Tblisi/branches')
     return [b['name'] for b in req.json()]
