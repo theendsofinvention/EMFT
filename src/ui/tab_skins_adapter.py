62c2348fa085a72486f4cd29be8b692b98403187
132nd-etcher <132nd-etcher@daribouca.net>
2017-05-09 12:52:27 +0200

    new: dev: add Adapter for skins tab

--- src/ui/tab_skins_adapter.py
+++ src/ui/tab_skins_adapter.py
@@ -1,3 +1,18 @@
 # coding=utf-8
+import abc
+
+from .itab import MainUiTabMethod
 
 TAB_NAME = 'Skins'
+
+
+class TabSkinsAdapter:
+    @MainUiTabMethod(TAB_NAME)
+    @abc.abstractmethod
+    def tab_skins_update_dcs_installs_combo(self):
+        """"""
+
+    @MainUiTabMethod(TAB_NAME)
+    @abc.abstractmethod
+    def tab_skins_show_skins_scan_result(self, scan_result):
+        """"""
