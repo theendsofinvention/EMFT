29095b223ff89be4d28e16bbd3b515418952f3e3
132nd-etcher <132nd-etcher@daribouca.net>
2017-05-14 16:17:08 +0200

    chg: redirect calls to Appveyor and Github to the new repo
    Following the transfer of ownership of the TRMT repo

--- src/misc/appveyor.py
+++ src/misc/appveyor.py
@@ -24,7 +24,7 @@ def get_latest_remote_version(branch='All') -> AVResult:
         req = requests.get(
             __url(
                 'projects',
-                '132nd-Entropy',
+                '132nd-vWing',
                 '132nd-virtual-wing-training-mission-tblisi'
             ),
         )
@@ -32,7 +32,7 @@ def get_latest_remote_version(branch='All') -> AVResult:
         req = requests.get(
             __url(
                 'projects',
-                '132nd-Entropy',
+                '132nd-vWing',
                 '132nd-virtual-wing-training-mission-tblisi',
                 'branch',
                 branch),
