ca31d7bcce2ff890c1150980c4129838b7abef98
132nd-etcher <132nd-etcher@daribouca.net>
2017-05-24 14:48:05 +0200

    fix: fixed laggy progress dialog when downloading a MIZ file

--- src/misc/downloader.py
+++ src/misc/downloader.py
@@ -9,11 +9,8 @@ from src.ui.main_ui_interface import I
 logger = make_logger(__name__)
 
 
-def download(url, local_file, progress_title: str, progress_text: str = '', file_size: int = None):
+def download(url, local_file, file_size: int = None):
     logger.info('downloading {} -> {}'.format(url, local_file))
-
-    # noinspection PyArgumentEqualDefault
-    I.progress_start(progress_title, length=100, label=progress_text)
 
     def hook(data):
         I.progress_set_value(int(float(data['percent_complete'])))
