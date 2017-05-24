73ea296992789081ccb265c5323b06a1f2e99a4f
132nd-etcher <132nd-etcher@daribouca.net>
2017-05-24 14:33:33 +0200

    fix: dev: added "overwrite" option to MIZ context

--- src/miz/miz.py
+++ src/miz/miz.py
@@ -30,7 +30,7 @@ class MizPath(Path):
 
 
 class Miz:
-    def __init__(self, path_to_miz_file, temp_dir=None, keep_temp_dir: bool = False):
+    def __init__(self, path_to_miz_file, temp_dir=None, keep_temp_dir: bool = False, overwrite: bool = False):
 
         self.miz_path = Path(path_to_miz_file)
 
@@ -48,6 +48,8 @@ class Miz:
 
         self.keep_temp_dir = keep_temp_dir
 
+        self.overwrite = overwrite
+
         self.tmpdir = Path(tempfile.mkdtemp('EMFT_'))
         logger.debug('temporary directory: {}'.format(self.tmpdir.abspath()))
 
@@ -61,7 +63,7 @@ class Miz:
 
     def __enter__(self):
         logger.debug('instantiating new Mission object as a context')
-        self.unzip()
+        self.unzip(self.overwrite)
         self._decode()
         return self
 
