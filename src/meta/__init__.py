b02fea36334330355966d215bf0f57dffbfd595c
132nd-etcher <132nd-etcher@daribouca.net>
2017-05-31 10:15:44 +0200

    fix: dev: add MetaProperty to package's __init__

--- src/meta/__init__.py
+++ src/meta/__init__.py
@@ -2,6 +2,7 @@
 
 from .meta import Meta
 from .meta_file import MetaFile
+from .meta_property import MetaProperty
 from .meta_property_with_default import MetaPropertyWithDefault
 from .meta_singleton import MetaSingleton
 from .meta_property_guid_with_default import MetaGUIDPropertyWithDefault
