3590ad6fe3cb8a2422b1f7ab1c41f48a596ddfc4
132nd-etcher <132nd-etcher@daribouca.net>
2017-05-31 09:54:07 +0200

    new: dev: add check for MainThread in MainUiInterface
    Calling a threaded UI method from the main python thread will raise a RuntimeError; this is potentially a very stupid thing to do, but it's ok for the time being

--- src/ui/main_ui_interface.py
+++ src/ui/main_ui_interface.py
@@ -10,6 +10,7 @@ from .tab_config_adapter import TabConfigAdapter
 from .tab_log_adapter import TabLogAdapter
 from .tab_roster_adapter import TabRosterAdapter
 from .tab_skins_adapter import TabSkinsAdapter
+import threading
 
 
 class MainUiMethod:
