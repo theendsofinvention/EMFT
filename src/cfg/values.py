9c5802d95986b9ca86c0dfd37d2c543d3a2cc617
132nd-etcher <132nd-etcher@daribouca.net>
2017-06-08 09:26:50 +0200

    fix: dev: fixed utils import in cfg.values (reverted from commit 144e497c9c9fcbd7dd4f03a5209473d3d8ed5423)

--- src/cfg/values.py
+++ src/cfg/values.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 import path
-from src.utils.custom_logging import make_logger
+from utils.custom_logging import make_logger
 
 from src.meta import MetaPropertyWithDefault, MetaGUIDPropertyWithDefault
 
