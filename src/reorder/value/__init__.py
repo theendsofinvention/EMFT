ed8b03ffde69bf5d041f697a826e2139bbfaec6f
132nd-etcher <132nd-etcher@daribouca.net>
2017-06-10 16:02:21 +0200

    chg: dev: update reorder.value.__init__

--- src/reorder/value/__init__.py
+++ src/reorder/value/__init__.py
@@ -1,7 +1,7 @@
 from .av_probe_result import AVProbeResult
-from .reorder_profile import ReorderProfile
 from .local_miz_file import LocalMizFile
+from .output_folder import OutputFolder
+from .output_folders import OutputFolders
 from .remote_av_repo import RemoteAVRepo
 from .remote_gh_repo import RemoteGHRepo
-
-ACTIVE_PROFILE = None
+from .reorder_profile import ReorderProfile
