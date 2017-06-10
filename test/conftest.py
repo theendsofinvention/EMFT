648268ae7ace4fc663c8cd9e0bff5136f6b37643
132nd-etcher <132nd-etcher@daribouca.net>
2017-06-10 16:22:05 +0200

    new: dev: add cleandir pytest fixture !minor

--- test/conftest.py
+++ test/conftest.py
@@ -1,1 +1,12 @@
 # coding=utf-8
+import os
+
+import pytest
+
+
+@pytest.fixture()
+def cleandir(tmpdir):
+    current_dir = os.getcwd()
+    os.chdir(str(tmpdir))
+    yield os.getcwd()
+    os.chdir(current_dir)
