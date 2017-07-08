b491689b69525222b20b446781b00c3d78b86df1
132nd-etcher <132nd-etcher@daribouca.net>
2017-07-08 13:39:34 +0200

    fix: dev: fix __all__ imports of FS

--- src/misc/fs/__init__.py
+++ src/misc/fs/__init__.py
@@ -4,4 +4,4 @@ from .dcs_installs import dcs_installs, DCSInstall
 from .dcs_skin import DCSSkin
 from .saved_games import saved_games_path
 
-__all__ = [dcs_installs, DCSSkin, DCSInstall, saved_games_path]
+__all__ = ['dcs_installs', 'DCSSkin', 'DCSInstall', 'saved_games_path']
