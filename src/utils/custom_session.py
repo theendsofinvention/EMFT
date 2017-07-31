c4474734a5b37c9964f0ebe9696f50fb17c2aa64
132nd-etcher <132nd-etcher@daribouca.net>
2017-07-31 13:17:03 +0200

    fix: dev: fix base custom session class

--- src/utils/custom_session.py
+++ src/utils/custom_session.py
@@ -1,15 +1,30 @@
 # coding=utf-8
+
+
+# noinspection PyPep8Naming
+class json_property:  # noqa: N801
+    """Wraps around properties to extract JSON key"""
+
+    def __init__(self, func):
+        self.key = func.__name__
+        self.func = func
+        self.__doc__ = func.__doc__
+
+    def __get__(self, obj, _):
+        if obj is None:  # pragma: no cover
+            return self
+        if not hasattr(obj, 'json'):
+            raise NotImplementedError('this object does not support JSON properties')
+        return obj.json.get(self.key, None)
+
+
 class JSONObject:
-    def __init__(self, json):
+    def __init__(self, json: dict):
         self._json = json
 
     @property
     def json(self) -> dict:
         return self._json
-
-    @property
-    def header(self):
-        return self.json['header']  # pragma: no cover
 
     def get_all(self):  # pragma: no cover
         ret = set()
@@ -35,20 +50,3 @@ class JSONObject:
                     getattr(a, 'print_all')(indent + '  ')
                 else:
                     print(indent, k, ':', a)
-
-
-# noinspection PyPep8Naming
-class json_property:
-    """Wraps around properties to extract JSON key"""
-
-    def __init__(self, func):
-        self.key = func.__name__
-        self.func = func
-        self.__doc__ = func.__doc__
-
-    def __get__(self, obj, _):
-        if obj is None:  # pragma: no cover
-            return self
-        if not hasattr(obj, 'json'):
-            raise NotImplementedError('this object does not support JSON properties')
-        return obj.json.get(self.key, None)
