a8e48e0371e13bbce2b522dff671ac50e8753066
132nd-etcher <132nd-etcher@daribouca.net>
2017-07-31 13:06:52 +0200

    chg: dev: rename monkey -> nice_exit

--- src/misc/nice_exit.py
+++ src/misc/nice_exit.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 
-def nice_exit(code: int = 0):
+def nice_exit(code: int = 0, *_):
     import os
     # Shameful monkey-patching to bypass windows being a jerk
     # noinspection PyProtectedMember
